271eabd7b0d6d0594fe72291289e5721ae21a8d0
fureurx <steam-product@hotmail.fr>
2024-05-21 14:16:10 +0200

    update truck class

--- src/ETS2.h
+++ src/ETS2.h
@@ -1,14 +1,15 @@
 class Truck
 {
 public:
-	char pad_0000[112]; //0x0000
-	float ChassiDmg; //0x0070
-	char pad_0074[132]; //0x0074
-	float WheelDmg; //0x00F8
-	char pad_00FC[36]; //0x00FC
-	float EngineDmg; //0x0120
-	float TransmissionDmg; //0x0124
-	float CabinDmg; //0x0128
-	float Fuel; //0x012C
-	char pad_0130[344]; //0x0130
+	char pad_0000[120]; //0x0000
+	float ChassiDmg; //0x0078
+	char pad_007C[164]; //0x007C
+	float WheelDmg; //0x0120
+	char pad_0124[36]; //0x0124
+	float EngineDmg; //0x0148
+	float TransmissionDmg; //0x014C
+	float CabinDmg; //0x0150
+	char pad_0154[12]; //0x0154
+	float Fuel; //0x0160
+	char pad_0164[796]; //0x0164
 };
